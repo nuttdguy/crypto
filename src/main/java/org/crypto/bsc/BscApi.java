4dfc838acae5d7257e38bd95f7c9d6292142646a
nuttdguy <pygnasakdev@gmail.com>
2022-12-26 14:36:50 -0600

    move impl from bscApi to bscService

--- src/main/java/org/crypto/bsc/BscApi.java
+++ src/main/java/org/crypto/bsc/BscApi.java
@@ -3,24 +3,14 @@ package org.crypto.bsc;
 import org.crypto.bsc.account.BscAccount;
 import org.crypto.bsc.account.BscAccountConfig;
 import org.crypto.bsc.account.BscAccountService;
-import org.json.JSONArray;
 import org.json.JSONException;
-import org.json.JSONObject;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
-import static org.crypto.util.JsonUtil.*;
 
 /* class to execute api requests to  retrieve data from https://api.bscscan.com/api/
-* ?/module/account
-* &action=balance
-* &address=0x.....
-* &apikey=eds3....
 * other actions found @ https://docs.bscscan.com/api-endpoints/accounts */
 public class BscApi {
 
@@ -32,39 +22,9 @@ public class BscApi {
         return bscAccountService.fetchApiResource(bscAccountConfig.getResourceUrl(), bscAccountConfig.getParamString(), bscAccountConfig.getApikey());
     }
 
-    /* from resource, extract the data into a list of bsc account instances
-    * throws JSONException if the resourceKey does not exist
-    * throws IOException when resourceStream cannot be converted to a JsonObject
-    * */
+    /* from resource stream, extract the data into a list of bsc account instances */
     public List<BscAccount> createListFrom(InputStream resourceStream, String resourceKey) throws JSONException, IOException {
-        List<BscAccount> accountEntries = new ArrayList<>();
-
-        // transform response into desired format, i.e. string, json, etc
-        JSONObject resource = toJsonObject(resourceStream);
-
-        // extract the data array from the json object
-        JSONArray resourceArray = resource.getJSONArray(resourceKey);
-
-        for (int i = 0; i < resourceArray.length(); i++) {
-
-            // create and then add bsc account instance
-            accountEntries.add(createAccountEntry(resourceArray.getJSONObject(i)));
-        }
-        return accountEntries;
+        return bscAccountService.createListFrom(resourceStream, resourceKey);
     }
-
-    /* creates an instance of bscAccount  */
-    private BscAccount createAccountEntry(JSONObject accountEntry) {
-        // if results is valid, extract all the keys and value types
-        Map<String, Object> keyPairs =
-                extractKeyPairs(accountEntry, new HashMap<>(), true);
-
-        // extract all values into String values
-        Map<String, String> keyValuePairs = mapObjectsToString(keyPairs);
-
-        // map key & value pairs into list of class instances
-        return bscAccountService.toAccountFromMap(keyValuePairs);
-    }
-
 
 }
--- src/main/java/org/crypto/bsc/account/BscAccountService.java
+++ src/main/java/org/crypto/bsc/account/BscAccountService.java
@@ -1,15 +1,22 @@
 package org.crypto.bsc.account;
 
 import org.crypto.HttpException;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import static org.crypto.bsc.account.BscAccountLabel.*;
 import static org.crypto.util.DataTypeUtil.*;
+import static org.crypto.util.JsonUtil.*;
 
 /* service class to fetch /?module=account resource from bsc explorer api */
 public class BscAccountService {
@@ -33,8 +40,44 @@ public class BscAccountService {
         return httpURLConnection.getInputStream();
     }
 
+    /* from resource stream, extract the data into a list of bsc account instances
+     * throws JSONException if the resourceKey does not exist
+     * throws IOException when resourceStream cannot be converted to a JsonObject
+     * */
+    public List<BscAccount> createListFrom(InputStream resourceStream, String resourceKey) throws JSONException, IOException {
+        List<BscAccount> accountEntries = new ArrayList<>();
+
+        // transform response into desired format, i.e. string, json, etc
+        JSONObject resource = toJsonObject(resourceStream);
+
+        // extract the data array from the json object
+        JSONArray resourceArray = resource.getJSONArray(resourceKey);
+
+        for (int i = 0; i < resourceArray.length(); i++) {
+
+            // create and then add bsc account instance
+            accountEntries.add(createAccountEntry(resourceArray.getJSONObject(i)));
+        }
+        return accountEntries;
+
+    }
+
+    /* creates an instance of bscAccount  */
+    public BscAccount createAccountEntry(JSONObject accountEntry) {
+        // if results is valid, extract all the keys and value types
+        Map<String, Object> keyPairs =
+                extractKeyPairs(accountEntry, new HashMap<>(), true);
+
+        // extract all values into String values
+        Map<String, String> keyValuePairs = mapObjectsToString(keyPairs);
+
+        // map key & value pairs into list of class instances
+        return toAccountFromMap(keyValuePairs);
+    }
+
+
     /* create an instance of BscAccount from a map entry */
-    public BscAccount toAccountFromMap(Map<String, String> accountEntry) {
+    protected BscAccount toAccountFromMap(Map<String, String> accountEntry) {
         return new BscAccount.AccountBuilder()
                 .withFunctionName(accountEntry.get(FUNCTION_NAME.label).replace(",", "::"))
                 .withBlockNumber(accountEntry.get(BLOCK_NUMBER.label))
